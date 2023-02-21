6bacfad0e08b28a9cad63ed83eec3c04f88e0700
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-21 19:32:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    //Add your code here
+    const prices = document.querySelectorAll(".price");
+    let sum = 0;
+    prices.forEach(price => sum += parseFloat(price.innerText));
+    const totalRow = document.createElement("tr");
+    const totalCell = document.createElement("td");
+    totalCell.colSpan = "2";
+    totalCell.innerText = `Total Price: $${sum.toFixed(2)}`;
+    totalRow.appendChild(totalCell);
+    document.querySelector("table").appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
